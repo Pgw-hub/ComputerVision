de89fb2dbc990e7551ee1620b3ffbd8028f496af
parkgeonwoo <80042706+Pgw-hub@users.noreply.github.com>
2022-09-07 17:05:20 +0900

    complete assignment2

---
+++ Lab/lab2/exercise2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main(){
+    Mat image;
+    float gamma = 10;
+    unsigned char pix[256];
+    image = imread("../data/lena.png",0);
+    if(image.empty()){
+        cout << "Could not open or find the image" << endl;
+        return -1;
+    }
+
+    //rotate
+    Mat trans_image;
+    rotate(image, trans_image, ROTATE_90_COUNTERCLOCKWISE);
+
+    //refer table
+    for(int i = 0; i < 256; ++i){
+        pix[i] = saturate_cast<uchar>(pow((float)(i / 255.0),gamma) * 255.0f);
+    }
+    
+    for(int row = 0; row < trans_image.rows; ++row){
+        for(int col = 0; col < trans_image.cols; ++col){
+            int val = trans_image.at<uchar>(row, col);
+            if( val < 127) trans_image.at<uchar>(row,col) = 255 - trans_image.at<uchar>(row,col);
+            else trans_image.at<uchar>(row,col) = pix[trans_image.at<uchar>(row,col)];  
+        }
+    }
+
+    imshow("gray image",image);
+    imshow("result",trans_image);
+    waitKey(0);
+}
